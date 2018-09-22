73b1c3b81d8be882e17427577a9693e8b5f124ee
Awais saeed <mawais2017@namal.edu.pk>
2018-09-22 14:12:45 +0000

    Changings in Task_3

--- Task_3.cpp
+++ Task_3.cpp
@@ -2,49 +2,58 @@
 
 using namespace std;
 
-float Calculate_savings(float ,float,float);
+float Calculate_savings(float,float,float);
 
 int main(int argc, char const *argv[]) {
-  float year,amount,further_deposit,initial_deposit,interest_rate,total_amount;
+  float No_of_year,amount,further_deposit,initial_deposit,interest_rate;
   char choice;
-  cout<<"\nEnter the initial_amount: ";
+  cout<<"\nEnter the Initial_amount: ";
   cin>>initial_deposit;
   while(true)
   {
-  cout<<"Do you want to deposit some amonut [y/n]";
+  cout<<"\nDo you want to deposit some amonut [y/n]: ";
   cin>>choice;
   if(choice=='y' || choice== 'Y')
   {
     cout<<"\nEnter the amount: ";
     cin>>further_deposit;
     initial_deposit+=further_deposit;
-    total_amount=initial_deposit;
-    cout<<"Enter N years"<<endl;
-    cin>>year;
+    cout<<"Enter No of years"<<endl;
+    cin>>No_of_year;
     cout<<"Enter interest rate: ";
     cin>>interest_rate;
-    total_amount=Calculate_savings(year,interest_rate,total_amount);
+    initial_deposit=Calculate_savings(No_of_year,interest_rate,initial_deposit);
   }
   else if(choice=='n' || choice== 'N')
   {
-    cout<<"Enter N years"<<endl;
-    cin>>year;
+    cout<<"Enter No of years"<<endl;
+    cin>>No_of_year;
     cout<<"Enter interest rate: ";
     cin>>interest_rate;
-    total_amount=initial_deposit;
-    total_amount=Calculate_savings(year,interest_rate,total_amount);
+    initial_deposit=Calculate_savings(No_of_year,interest_rate,initial_deposit);
   }
-  cout<<"Total_savings: "<<total_amount;
+  else
+  cout<<"\nwrong choice"<<endl;
+  cout<<"\nTotal_savings: "<<initial_deposit;
+  cout<<"\nDO you want to Quit [y/n]: ";
+  cin>>choice;
+  if(choice=='Y' || choice=='y')
+  break;
+  else
+  continue;
 }
   return 0;
 }
-float Calculate_savings(float year,float interest_rate,float total_amount)
+
+
+float Calculate_savings(float No_of_year,float interest_rate,float initial_deposit)
 {
-  int i,profit=0;
-  for(i=1;i<year;i++)
+  int i;
+  float profit=0;
+  for(i=1;i<=No_of_year;i++)
   {
-    profit+=interest_rate*total_amount;
-    total_amount+=profit;
+    profit=(interest_rate/100)*initial_deposit;
+    initial_deposit+=profit;
   }
-  return total_amount;
+  return initial_deposit;
 }
